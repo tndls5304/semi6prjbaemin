612fff2cc5f6eed97fdc4b5450ef95456857aff1
DESKTOP-OSQ1V9F\user1 <hongjy3458@gmail.com>
2024-05-27 17:28:28 +0900

    이그노어 추가설정

--- group6bmprj/src/main/java/com/kh/baemin/store/controller/StoreAdmitController.java
+++ group6bmprj/src/main/java/com/kh/baemin/store/controller/StoreAdmitController.java
@@ -13,14 +13,12 @@ public class StoreAdmitController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		
 	}
 
 }
