9e8027a7f49b87ca9a71617752e6121ef66c1b29
DESKTOP-7MHC037\user1 <geuna_1019@naver.com>
2024-05-29 14:13:06 +0900

    conf.xml수정하고 admin 회원관리 추가함

--- group6bmprj/src/main/java/com/kh/baemin/admin/service/AdminManageUserService.java
+++ group6bmprj/src/main/java/com/kh/baemin/admin/service/AdminManageUserService.java
@@ -1,5 +1,28 @@
 package com.kh.baemin.admin.service;
 
+import static com.kh.baemin.db.SqlSessionTemplate.getSqlSession;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.kh.baemin.admin.dao.AdminManageUserDao;
+import com.kh.baemin.member.vo.MemberVo;
+
 public class AdminManageUserService {
+	
+	private final AdminManageUserDao dao;
+	public AdminManageUserService() {
+		this.dao=new AdminManageUserDao();
+	}
+	
+	public List<MemberVo> getUserListById(String userId) {
+		SqlSession ss=getSqlSession();
+		List<MemberVo>voList=dao.getUserListById(ss,userId);
+		ss.close();
+		
+		return voList;
+		
+	}
 
 }
