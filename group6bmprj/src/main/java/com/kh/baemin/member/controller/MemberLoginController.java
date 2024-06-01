494e953a9f37efcc467688d7d7540bc285237809
DESKTOP-N09NIKN\user <dlwngud0929@naver.com>
2024-06-01 20:48:01 +0900

    ㅇ

--- group6bmprj/src/main/java/com/kh/baemin/member/controller/MemberLoginController.java
+++ group6bmprj/src/main/java/com/kh/baemin/member/controller/MemberLoginController.java
@@ -51,8 +51,8 @@ System.out.println("loginvo: " + loginMemberVo);
 
 			// 로그인 성공
 			 
-			req.setAttribute("alertMsg", "로그인 성공!!!");
-			req.setAttribute("loginMemberVo", loginMemberVo);
+			session.setAttribute("alertMsg", "로그인 성공!!!");
+			session.setAttribute("loginMemberVo", loginMemberVo);
 			System.out.println(loginMemberVo);
 			resp.sendRedirect("/baemin/member/home"); //홈 페이지로 리디렉트
 		} catch (Exception e) {
